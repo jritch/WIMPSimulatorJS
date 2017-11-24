24b22acee343efcb32c0cdacccb311ae8ce49483
John Kanji <arrggee@gmail.com>
2017-11-24 15:54:31 -0500

    Improve AuRe GUI. Convert CSS to SCSS

--- aure.js
+++ aure.js
@@ -9,26 +9,39 @@ $(function(){
     };
     var currState = State.IDLE;
 
+    function setState(s) {
+        currState = s;
+        if (currState == State.IDLE) {
+            aure.find('#aure-editor').addClass('small')
+            recordHead.find('.icon').addClass('hidden');
+            recordHead.removeClass('edit').addClass('record');
+        }
+        else if (currState == State.RECORD) {
+            recordHead.find('.icon').addClass('hidden');
+            recordHead.find('.fa-stop').removeClass('hidden');
+        }
+        else {
+            aure.find('#aure-editor').removeClass('small')
+            recordHead.removeClass('record').addClass('edit');
+            recordHead.find('.icon').addClass('hidden');
+            recordHead.find('.fa-play').removeClass('hidden');
+        }
+    }
+
     aure.draggable({handle: '.handle'});
     recordHead.draggable({cancel: '#record-head'})
     .click(function(e) {
         if (currState == State.IDLE) {
-            currState = State.RECORD;
-            recordHead.find('.icon').hide();
-            recordHead.find('#stop-icon').show();
+            setState(State.RECORD);
         }
         else if (currState == State.RECORD) {
-            currState = State.EDIT;
-            aure.find('#aure-editor').removeClass('small')
-            recordHead.removeClass('record').addClass('edit');
-            recordHead.find('.icon').hide();
-            recordHead.find('#play-icon').show();
+            setState(State.EDIT)
         }
-        else {
-            currState = State.IDLE;
-            aure.find('#aure-editor').addClass('small')
-            recordHead.find('.icon').hide();
-            recordHead.removeClass('edit').addClass('record');
+        else if (currState == State.EDIT && !recordHead.hasClass('disabled')) {
+            setState(State.IDLE);
         }
     })
+    aure.find('#aure-cancel').click(function(e) { setState(State.IDLE) })
+
+    setState(State.EDIT);
 });
