bf2b9db5bf9577d6e7cbc8f693a366327ad32ee1
John Kanji <johnkanji@visitor115.visitor.dgp.sandbox>
2017-11-23 23:55:19 -0500

    Add more UI for AuRe panel

--- aure.js
+++ aure.js
@@ -1,3 +1,32 @@
 $(function(){
-    $('#aure').draggable()
+    let aure = $('#aure');
+    let recordHead = $('#record-head')
+
+    let State = {
+        IDLE: 0,
+        RECORD: 1,
+        EDIT: 2,
+    };
+    var currState = State.IDLE;
+
+    aure.draggable({handle: '.handle'});
+    recordHead.draggable({cancel: '#record-head'})
+    .click(function(e) {
+        if (currState == State.IDLE) {
+            currState = State.RECORD;
+            recordHead.find('.icon').hide();
+            recordHead.find('#stop-icon').show();
+        }
+        else if (currState == State.RECORD) {
+            currState = State.EDIT;
+            recordHead.removeClass('record').addClass('edit');
+            recordHead.find('.icon').hide();
+            recordHead.find('#play-icon').show();
+        }
+        else {
+            currState = State.IDLE;
+            recordHead.find('.icon').hide();
+            recordHead.removeClass('edit').addClass('record');
+        }
+    })
 });
