c9b428337df5ab36853d423d920b03d92a456ea5
John Kanji <arrggee@gmail.com>
2017-12-03 21:00:16 -0500

    Add intial recording and playback functionality

--- aure.js
+++ aure.js
@@ -1,85 +1,141 @@
 jQuery = $;
 
 class Action {
-    constructor(name, icon, list) {
-        this.name = name;
-        this.icon = icon;
-        this.list = list;
+    constructor(args) {
+        this.title = args.title;
+        this.icon = args.icon;
+        this.type = args.type;
+        this.data = args.data;
+        this.list = args.list;
     }
 }
 let actionList = [
-    new Action('Test Action 1', 'copy'),
-    new Action('Test Action 2', 'paste'),
-    new Action('Test Action 3', 'file-excel-o'),
+    new Action({
+        title: 'Test Action 1', 
+        icon: 'copy'
+    }),
+    new Action({
+        title: 'Test Action 1', 
+        icon: 'paste'
+    }),
+    new Action({
+        title: 'Test Action 1', 
+        icon: 'file-excel-o'
+    })
 ];
 
-let State = {
-    IDLE: 0,
-    RECORD: 1,
-    EDIT: 2,
-};
-var currState = State.IDLE;
+class Aure {
+    constructor(args) {
+        this.$aure = $(args.el);
+        this.$recordHead = $(args.recordHead);
+        this.State = {
+            IDLE: 0,
+            RECORD: 1,
+            EDIT: 2,
+        }
+        this.currState = this.State.IDLE;
+        this.actionList = [];
 
-function setState(s, aure, recordHead) {
-    if (s == State.IDLE) {
-        actionList = [];
-        aure.find('#aure-action-editor').addClass('small')
-        recordHead.find('.icon').addClass('hidden');
-        recordHead.removeClass('edit').addClass('record');
+        this.$aure.draggable({handle: '.handle'});
+        this.$recordHead.draggable({cancel: '#record-head'})
+        .click(e => {
+            if (this.currState == this.State.IDLE) {
+                this.setState(this.State.RECORD);
+            }
+            else if (this.currState == this.State.RECORD) {
+                this.buildActionList()
+                this.setState(this.State.EDIT)
+            }
+            else if (this.currState == this.State.EDIT && !this.$recordHead.hasClass('disabled')) {
+                this.setState(this.State.IDLE);
+            }
+        })
+        this.$aure.find('#aure-cancel').click(e => { this.setState(this.State.IDLE) })
+        this.$aure.find('#aure-save').click(e => {
+            // $('#t1_1').on('copy', () => {console.log('copy')})
+            this.buildActionList(actionList);
+        });
     }
-    else if (s == State.RECORD) {
-        recordHead.find('.icon').addClass('hidden');
-        recordHead.find('.fa-stop').removeClass('hidden');
+    setState(s) {
+        if (s == this.State.IDLE) {
+            this.actionList = [];
+            this.$aure.find('#aure-action-editor').addClass('small')
+            this.$recordHead.find('.icon').addClass('hidden');
+            this.$recordHead.removeClass('edit')
+        }
+        else if (s == this.State.RECORD) {
+            this.$recordHead.find('.icon').addClass('hidden');
+            this.$recordHead.find('.fa-stop').removeClass('hidden');
+            this.$recordHead.addClass('record');
+        }
+        else {
+            this.$aure.find('#aure-action-editor').removeClass('small')
+            this.$recordHead.removeClass('record').addClass('edit');
+            this.$recordHead.find('.icon').addClass('hidden');
+            this.$recordHead.find('.fa-play').removeClass('hidden');
+        }
+        this.currState = s;
     }
-    else {
-        aure.find('#aure-action-editor').removeClass('small')
-        recordHead.removeClass('record').addClass('edit');
-        recordHead.find('.icon').addClass('hidden');
-        recordHead.find('.fa-play').removeClass('hidden');
+    buildActionList(acs) {
+        if (acs == undefined) acs = this.actionList;
+        let $el = acs.reduce((acc, a) => {
+            let listClasses = (a) => {
+                if (a.list == undefined) return ''
+                else if (a.list.length == 0) return 'listable needs-list'
+                else return 'listable has-list'
+            }
+            let $a = $(`
+                <div class="aure-action ${listClasses(a)}">
+                    <div class="aure-action-icon icon">
+                        <i class="fa fa-${a.icon}"></i>
+                    </div>
+                    <div>
+                        <p>${a.title}</p>
+                    </div>
+                    <div class="aure-add-list icon">
+                        <i class="fa fa-plus-square"></i>
+                    </div>
+                </div>
+            `)
+            $a.find('.aure-add-list i').click(e => {
+                this.buildFileList(a)
+                this.$aure.find('#aure-list').toggleClass('visible')
+            })
+            acc.append($a);
+            return acc;
+        }, $('<div>', { id: 'aure-actions' }));
+        $el.find('#aure-list-items').sortable();
+        $el.find('#aure-list-items').disableSelection();
+        $el.find('#aure-actions').sortable();
+        $el.find('#aure-actions').disableSelection();
+        $('#aure-actions').replaceWith($el);
     }
-    currState = s;
+    buildFileList(a) {
+        let $el;
+        if (a.list == undefined || a.list.length == 0) {
+            $el = $(`<input type="button" class="aure-list-items" class="btn" value="Add files">`)
+        }
+        else {
+            $el = a.list.reduce()
+        }
+        console.log(this.$aure.find('#aure-list'));
+        this.$aure.find('.aure-list-items').remove();
+        this.$aure.find('#aure-list').append($el);
+    }
+    addAction(a) {
+        if (this.currState == this.State.RECORD) {
+            this.actionList.push(a);
+            this.$recordHead.addClass('flash');
+            setTimeout(() => this.$recordHead.removeClass('flash'), 250)
+        }
+    }
 }
 
-function buildActionList(as) {
-    let $el = as.reduce((acc, a) => {
-        $a = $(document.createElement('div')).addClass('aure-action');
-        $a.append($('<div>', { class: 'aure-action-icon icon' })
-            .append($('<i>', { class: `fa fa-${a.icon}` })));
-        $a.append($('<div>').append($('<p>').text(a.name)));
-        acc.append($a);
-        return acc;
-    }, $('<div>', { id: 'aure-actions' }));
-    $('#aure-actions').replaceWith($el);
-}
+var aure;
 
 $(function(){
-    let aure = $('#aure');
-    let recordHead = $('#record-head')
-
-    aure.draggable({handle: '.handle'});
-    recordHead.draggable({cancel: '#record-head'})
-    .click(function(e) {
-        if (currState == State.IDLE) {
-            setState(State.RECORD, aure, recordHead);
-        }
-        else if (currState == State.RECORD) {
-            setState(State.EDIT, aure, recordHead)
-        }
-        else if (currState == State.EDIT && !recordHead.hasClass('disabled')) {
-            setState(State.IDLE, aure, recordHead);
-        }
-    })
-    aure.find('#aure-cancel').click(function(e) { setState(State.IDLE, aure, recordHead) })
-    aure.find('#aure-save').click(e => {
-        // $('#t1_1').on('copy', () => {console.log('copy')})
-        buildActionList(actionList);
+    aure = new Aure({
+        el: '#aure',
+        recordHead: '#record-head'
     });
-    aure.find('.aure-add-list i').click(e => {
-        // buildFileList()
-        aure.find('#aure-list').toggleClass('visible')
-    })
-    $('#aure-list-items').sortable();
-    $('#aure-list-items').disableSelection();
-    $('#aure-actions').sortable();
-    $('#aure-actions').disableSelection();
 });
--- treeEditable.js
+++ treeEditable.js
@@ -1,285 +1,260 @@
-
-function blur_event (id)   {
-  console.log("???????")
-  slide_information["text"][id] = $("#slide_txt").text();
-  // TODO: Add to action list an append text action
-  // #TODO: Add to action list an insert action
-  a = new Action('Add slide text', 'font');
-  actionList.push(a)
+ var slide_information = {
+	text:[],
+	image_titles:[]
 }
 
- var slide_information = {
-     text:[],
-     image_titles:[]
-   }
-
 var ppt_config = {
-    layout: {
-        name: 'layout',
-        padding: 0,
-        panels: [
-            { type: 'left', size: 200, resizable: false, minSize: 120, title: 'Slides'  },
-            { type: 'main', minSize: 550, overflow: 'hidden' }
-        ]
-    },
-    sidebar: {
-        name: 'sidebar',
-        nodes: [
-                { id: '1', text: 'Slide 1', img: 'MyIcon1', selected: true },
-        ],
+	layout: {
+		name: 'layout',
+		padding: 0,
+		panels: [
+			{ type: 'left', size: 200, resizable: false, minSize: 120, title: 'Slides'	},
+			{ type: 'main', minSize: 550, overflow: 'hidden' }
+		]
+	},
+	sidebar: {
+		name: 'sidebar',
+		nodes: [
+			{ id: '1', text: 'Slide 1', img: 'MyIcon1', selected: true },
+		],
+		onLoad: function (event) { sidebar.onClick(event); },
+		onClick: function (event) {
+			if (event) {
+				var id = Number(event.target) - 1;
+			}
+			else {
+				var id = 0;
+			}
 
-        onLoad: function (event) {sidebar.onClick(event);},
+			w2ui.layout.content('main', `
+				<div style="width:80%;padding:20px;height:80%; margin: auto; background-color:white">
+					<div class="ppt-text" style="padding: 10px;border: 1px; border-style:dashed" contenteditable="true">
+						<span id=slide_txt>Edit to Insert Text.</span>
+					</div>
+					<br>
+					<div class="ppt-image" style="padding: 10px; border: 1px; border-style:dashed;height:90%">
+						<span id="prompt">Click to Insert Image.</span>
+						<br>
+						<img style="margin:auto;" id="active_img">
+					</div>
+				</div>
+			`
+			);
 
-        onClick: function (event) {
-            if (event) {
-                var id = Number(event.target) - 1;
-            }
-            else {
-              var id = 0;
-            }
+			$(w2ui.layout.el('main'))
+				.css({
+					'border-left': '1px solid silver',
+					'padding ': '100px'
+				});
+			$('.ppt-text').blur(e => {
+				slide_information['text'][id] = $('.ppt-text').text().trim();
+				aure.addAction(new Action({
+					type: 'ppt-insert-text',
+					data: {
+						id: id,
+						text: slide_information['text'][id]
+					},
+					title: 'Add slide text', 
+					icon: 'font'
+				}))
+			})
+			$('.ppt-image').click(e => $('#dialog').dialog('open'));
 
-            w2ui.layout.content('main', '<div style="width:80%;padding:20px;height:80%; margin: auto; background-color:white">'+
-              '<div style="padding: 10px;border: 1px; border-style:dashed" onblur="blur_event('+String(id)+ ')" contenteditable="true">'
-              + "<span id=slide_txt>Edit to Insert Text.</span>" +  '</div><br><div onclick="$(\'#dialog\' ).dialog( \'open\' );" style="padding: 10px; border: 1px; border-style:dashed;height:90%">'
-              + "<span id='prompt'>Click to Insert Image.</span>" +'<br><img style="margin:auto;" id="active_img"></div></div>'
-            );
+			if (event) {
+				// show an image if it's been added
+				if (slide_information['image_titles'].length > id){
+					$('#active_img').attr('src','data/img/' + slide_information['image_titles'][id]);
+					$('#prompt').remove();
+				}
 
-            $(w2ui.layout.el('main'))
-                .css({
-                    'border-left': '1px solid silver',
-                    'padding ': '100px'
-                });
-
-            if (event){
-
-              // show an image if it's been added
-              if (slide_information["image_titles"].length > id){
-                //console.log("YEAH WE IN THIS CASE BOY")
-                $("#active_img").attr("src",slide_information["image_titles"][id]);
-                $("#prompt").remove();
-              }
-
-              // Show the correct text if it's been added
-              if (slide_information["text"].length > id){
-                $("#slide_txt").text(slide_information["text"][id]);
-              }
-
-            }
-        }
-    },
-
+				// Show the correct text if it's been added
+				if (slide_information['text'].length > id) {
+					$('#slide_txt').text(slide_information['text'][id]);
+				}
+			}
+		}
+	},
 };
 
 var toolbar = {
-  name: 'toolbar',
-  items : [
-    {type:'button', caption: 'Insert Slide', img: 'icon-folder'}
-  ],
-  onClick: function(event) {
-    num = String(w2ui["sidebar"].nodes.length + 1)
-    w2ui['sidebar'].add({ id: num, text: 'Slide '+ num, img: 'MyIcon1', })
-    a = new Action('Insert a slide', 'copy');
-    actionList.push(a);
-  }
+	name: 'toolbar',
+	items : [
+		{type:'button', caption: 'Insert Slide', img: 'icon-folder'}
+	],
+	onClick: function(event) {
+		num = String(w2ui['sidebar'].nodes.length + 1)
+		w2ui['sidebar'].add({ id: num, text: 'Slide '+ num, img: 'MyIcon1', })
+		aure.addAction(new Action({
+			type: 'ppt-insert-slide',
+			data: num,
+			title: 'Insert a slide',
+			icon: 'copy',
+		}));
+	}
 };
 
 var config = {
-  layout: {
-      name: 'myLayout',
-      padding: 0,
-      panels: [
-          { type: 'main', minSize: 550, overflow: 'hidden', title: '' }
-      ]
-  },
-  notepad: {
-    content: ""
-  },
-  pdfviewer: {
-    HTML: "<textarea id='PDFViewer' rows='50' cols='100'></textarea>",
-    content: ""
-  },
-
+	layout: {
+		name: 'myLayout',
+		padding: 0,
+		panels: [
+			{ type: 'main', minSize: 550, overflow: 'hidden', title: '' }
+		]
+	},
+	notepad: {
+		content: ''
+	}
 };
 
 $(function(){
-  $("#tree").fancytree({
-    extensions: ["edit"],
-    selectMode: 2,
-    source: [  // Typically we would load using ajax instead...
+	$('#tree').fancytree({
+		extensions: ['edit'],
+		selectMode: 2,
+		source: [
+			{title: 'TV Shows', folder: true, expanded: true, children: [
+				{title: 'The.Good.Place.S01E01.HDTV.HebSubs.XviD-AFG.avi', key: 'vlc_1'},
+				{title: 'The.Good.Place.S01E02.1080p.HDTV.x264-CROOKS.mp4', key: 'vlc_2'},
+				{title: 'The.Good.Place.S01E03.HDTV.x264-KILLERS.mp4', key: 'vlc_3'},
+				{title: 'The.Walking.Dead.S08E01.1080p.WEB-DL.DD5.1.H264-RARBG.mp4', key: 'vlc_4'}
+			]},
+			{title: 'CHI Papers', folder: true, children: [
+				{title: 'PAPER_TITLES.txt'},
+				{title: 'p76-wilson.pdf'},
+				{title: 'p255-lobiaonco.pdf'},
+				{title: 'W10-Baecker.pdf'},
+				{title: 'W10-Benjamin.pdf'},
+				{title: 'W10-Consolvo.pdf'},
+				{title: 'W10-Gerling.pdf'},
+				{title: 'W10-Hwang.pdf'},
+				{title: 'W10-Lazar-1.pdf'},
+				{title: 'W10-Lazar-2.pdf'},
+			]},
+			{title: 'Screenshots', folder: true, children: [
+				{title: 'SCREENSHOTS.ppt'},
+				{title: '1.jpg'},
+				{title: '2.jpg'},
+				{title: '3.jpg'},
+				{title: '4.jpg'},
+				{title: '5.jpg'},
+				{title: '6.jpg'},
+				{title: '7.jpg'},
+				{title: '8.jpg'},
+				{title: '9.jpg'},
+				{title: '10.jpg'},
+				{title: '11.jpg'},
+				{title: '12.jpg'},
+				{title: '13.jpg'},
+				{title: '14.jpg'},
+			]}
+		],
+		edit: {
+			triggerStart: ['f2', 'dblclick', 'shift+click', 'mac+enter']
+		},
+		activate: function(event, data) {
+			// save notepad state
+			config.notepad.content = $('#notepad').length ? $('#notepad').val() : config.notepad.content;
+			
+			if (data.node.title == 'PAPER_TITLES.txt') {
+				console.log('Open Notepad!');
+				if(w2ui['layout']) {
+					w2ui['layout'].destroy();
+					w2ui['sidebar'].destroy();
+					w2ui['toolbar'].destroy();
+				}
+				html = `<textarea id="notepad" style="height:100%;width:100%">${config.notepad.content}</textarea>`;
+				w2ui.myLayout.set('main',{title:'Notepad'});
+				w2ui.myLayout.content('main', html);
+			}
+			else if (data.node.title == 'SCREENSHOTS.ppt') {
+				console.log('Open Powerpoint!');
+				html = `
+					<div class="row">
+						<div class="col-sm-12">
+							<div id="toolbar"></div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<div id="main" style="height: 700px"></div>
+						</div>
+					</div>
+				`;
+				w2ui.myLayout.set('main', {title:'Screenshots'});
+				w2ui.myLayout.content('main', html);
 
-      {title: "TV Shows", folder: true, expanded: true, children: [
-        {title: "The.Good.Place.S01E01.HDTV.HebSubs.XviD-AFG.avi", key: "vlc_1"},
-        {title: "The.Good.Place.S01E02.1080p.HDTV.x264-CROOKS.mp4", key: "vlc_2"},
-        {title: "The.Good.Place.S01E03.HDTV.x264-KILLERS.mp4", key: "vlc_3"},
-        {title: "The.Walking.Dead.S08E01.1080p.WEB-DL.DD5.1.H264-RARBG.mp4", key: "vlc_4"}
-      ]},
-      {title: "CHI Papers", folder: true, children: [
-        {title: "PAPER_TITLES.txt"},
-        {title: "p76-wilson.pdf"},
-        {title: "p255-lobiaonco.pdf"},
-        {title: "W10-Baecker.pdf"},
-        {title: "W10-Benjamin.pdf"},
-        {title: "W10-Consolvo.pdf"},
-        {title: "W10-Gerling.pdf"},
-        {title: "W10-Hwang.pdf"},
-        {title: "W10-Lazar-1.pdf"},
-        {title: "W10-Lazar-2.pdf"},
-      ]},
-      {title: "Screenshots", folder: true, children: [
-        {title: "SCREENSHOTS.ppt"},
-        {title: "1.png"},
-        {title: "2.png"},
-        {title: "3.png"},
-        {title: "4.png"},
-        {title: "5.png"},
-        {title: "6.png"},
-        {title: "7.png"},
-        {title: "8.png"},
-        {title: "9.png"},
-        {title: "10.png"},
-        {title: "11.png"},
-        {title: "12.png"},
-        {title: "13.png"},
-        {title: "14.png"},
-      ]}
-    ],
-    lazyLoad: function(event, data) {
-      data.result = { url: "ajax-sub2.json", debugDelay: 1000 };
-    },
-    edit: {
-      triggerStart: ["f2", "dblclick", "shift+click", "mac+enter"],
-      beforeEdit: function(event, data){
-        // Return false to prevent edit mode
-      },
-      edit: function(event, data){
-        // Editor was opened (available as data.input)
-      },
-      beforeClose: function(event, data){
-        // Return false to prevent cancel/save (data.input is available)
-        console.log(event.type, event, data);
-        if( data.originalEvent.type === "mousedown" ) {
-          // We could prevent the mouse click from generating a blur event
-          // (which would then again close the editor) and return `false` to keep
-          // the editor open:
-//                  data.originalEvent.preventDefault();
-//                  return false;
-          // Or go on with closing the editor, but discard any changes:
-//                  data.save = false;
-        }
-      },
-      save: function(event, data){
-        // Save data.input.val() or return false to keep editor open
-        console.log("save...", this, data);
-        // Simulate to start a slow ajax request...
-        //setTimeout(function(){
-          //$(data.node.span).removeClass("pending");
-          // Let's pretend the server returned a slightly modified
-          // title:
-          //data.node.setTitle(data.node.title + "!");
-        //}, 2000);
-        // We return true, so ext-edit will set the current user input
-        // as title
-        return true;
-      },
-      close: function(event, data){
-        // Editor was removed
-        if( data.save ) {
-          // Since we started an async request, mark the node as preliminary
-          $(data.node.span).addClass("pending");
-        }
-      }
-    },
-    activate: function(event, data){
-        var textArea = document.getElementById("notepad");
-        if (textArea != null) config.notepad.content = textArea.value;
-        if (data.node.title == 'PAPER_TITLES.txt') {
-          if(w2ui['layout']) {
-            w2ui['layout'].destroy();
-            w2ui['sidebar'].destroy();
-            w2ui['toolbar'].destroy();
-          }
-          console.log("Open Notepad!");
-          html = "<textarea id='notepad' style='height:100%;width:100%'>" + config.notepad.content + "</textarea>";
-          w2ui.myLayout.set('main',{title:"Notepad"});
-          w2ui.myLayout.content('main', html);
-        }
-        else if (data.node.title == "SCREENSHOTS.ppt"){
-          console.log("Open Powerpoint!");
-          html = '<div class="row"><div class="col-sm-12"><div id="toolbar"></div></div></div><div class="row"><div class="col-sm-12"><div id="main" style="height: 700px"></div></div></div>';
-          w2ui.myLayout.set('main',{title:"Screenshots"});
-          w2ui.myLayout.content('main', html);
+				$('#main').w2layout(ppt_config.layout);
+				w2ui.layout.content('left', $().w2sidebar(ppt_config.sidebar));
 
-          $('#main').w2layout(ppt_config.layout);
-          w2ui.layout.content('left', $().w2sidebar(ppt_config.sidebar));
-          // in memory initialization
-          $().w2grid(ppt_config.grid2);
+				$('#toolbar').w2toolbar(toolbar);
+				w2ui['sidebar'].on('render',function(event) {
+					ppt_config.sidebar.onClick();
+				})
 
-          $('#toolbar').w2toolbar(toolbar);
-          w2ui["sidebar"].on('render',function(event){
+				$('#insert_button').on('click', function () {
+					$('#dialog').dialog('close');
+					$('#prompt').remove();
+					id = Number(w2ui['sidebar'].selected)-1;
+					//TODO: change this to the correct image title
+					if($('#tree').fancytree('getActiveNode')) {
+						slide_information['image_titles'][id]	= $('#tree').fancytree('getActiveNode').title
+					}
+					else {
+						slide_information['image_titles'][id] = 'ex.gif'
+					}
+					$('#active_img').attr('src','data/img/' + slide_information['image_titles'][id])
 
-            ppt_config.sidebar.onClick();})
-          w2ui["sidebar"].on("refresh", function(event){
-            $(".hack").remove();
-            $(".MyIcon1").after("<br class='hack' clear=all>");
+					// #TODO: Add to action list an insert action
+					aure.addAction(new Action({
+						type: 'ppt-insert-image',
+						data: {
+							id: id,
+							src: slide_information['image_titles'][id]
+						},
+						title: 'Insert a slide image', 
+						icon: 'paperclip',
+						list: []
+					}))
+					console.log(aure.actionList);
+				});
+			}
+			else if (data.node.title.indexOf('.pdf') !== -1) {
+				if (w2ui['layout']) {
+					w2ui['layout'].destroy();
+					w2ui['sidebar'].destroy();
+					w2ui['toolbar'].destroy();
+				}
+				console.log('Opening PDF!');
+				w2ui.myLayout.set('main',{title:'PDF Viewer'});
+				// Get the content to display on the PDF viewer
+				var pdfContent = `
+					<div id="pdf-content" style="height:100%">
+						<object style="height:100%;width:100%" data="data/pdfs/CHI2011/1.html">
+							Your browser doesn't support object tag :(
+						</object>
+					</div>`;
+				w2ui.myLayout.content('main', pdfContent);
+			}
+		},
+		// click: function(e, data) {
+		// 	if( !data.node.folder ) {
+		// 		console.log('onclick');
+		// 		data.node.toggleSelected();
+		// 	}
+		// }
+	});
 
-          })
+	$('#myApps').w2layout(config.layout);
+	w2ui.myLayout.content('main', '');
 
-          $('#insert_button').on("click", function () {
-            $("#dialog").dialog("close");
-            $("#prompt").remove();
-            id = Number(w2ui['sidebar'].selected)-1;
-            //TODO: change this to the correct image title
-            if($("#tree").fancytree("getActiveNode")) {
-              slide_information["image_titles"][id]  = $("#tree").fancytree("getActiveNode").title
-            }
-            else {
-              slide_information["image_titles"][id] = "ex.gif"
-            }
-            $("#active_img").attr("src",slide_information["image_titles"][id])
-
-            // #TODO: Add to action list an insert action
-            a = new Action('Insert a slide image', 'paperclip');
-            actionList.push(a)
-          });
-
-
-        }
-         else if (data.node.title.indexOf(".pdf") !== -1) {
-          if(w2ui['layout']) {
-            w2ui['layout'].destroy();
-            w2ui['sidebar'].destroy();
-            w2ui['toolbar'].destroy();
-          }
-          console.log("Opening PDF!");
-          w2ui.myLayout.set('main',{title:"PDF Viewer"});
-          // Get the content to display on the PDF viewer
-          var pdfcontent = "<div id='PDFContent' style='height:100%'><object style='height:100%;width:100%' data='data/pdfs/CHI2011/index.html'>Your browser doesn't support object tag :(</object></div>";
-          w2ui.myLayout.content('main', pdfcontent);
-        }
-      },
-      click: function(e, data) {
-   if( ! data.node.folder ){
-     data.node.toggleSelected();
-   }
-  }
-  });
-});
-
-$(function () {
-  $('#myApps').w2layout(config.layout);
-  w2ui.myLayout.content('main', "");
-});
-
-$(function() {
-  $( "#dialog" ).dialog({
-    autoOpen: false,
-    show: {
-      effect: "blind",
-      duration: 100
-    },
-    hide: {
-      effect: "explode",
-      duration: 1000
-    }
-  });
+	$('#dialog').dialog({
+		autoOpen: false,
+		show: {
+			effect: 'blind',
+			duration: 100
+		},
+		hide: {
+			effect: 'explode',
+			duration: 1000
+		}
+	});
 });
